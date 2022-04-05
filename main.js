ba9ac1b6b50acfda57c18d8550b63906c4275f7b
George Peralta <lucasperalta1993@hotmail.com>
2022-04-05 14:06:51 -0300

    Inicial
    Iniciando com vue

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
+            premium: true
+        }
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        // solution
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+                if (index > -1) {
+                    this.cart.splice(index, 1)
+                }
+        }
+    }        
+})
